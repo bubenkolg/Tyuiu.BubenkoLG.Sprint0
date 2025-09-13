6183b6880714106b1c001893fe5e311aa4f6316d
Людмила Бубенко <Tyuiu.bubenkolg@outlook.com>
2025-09-13 17:56:03 +0500

    Task1

--- Tyuiu.BubenkoLG.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.BubenkoLG.Sprint0.Task1.V0/Program.cs
@@ -10,6 +10,11 @@ namespace Tyuiu.BubenkoLG.Sprint0.Task1.V0
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
+            string name = "Людмила";
+            int age = 18;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет.");
+            Console.ReadKey();
         }
     }
 }
