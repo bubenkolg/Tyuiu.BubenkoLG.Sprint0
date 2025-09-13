69eb4d7a5607e476b1832132275bce6e5e100bce
Людмила Бубенко <Tyuiu.bubenkolg@outlook.com>
2025-09-13 19:29:10 +0500

    Task5

--- Tyuiu.BubenkoLG.Sprint0.Task5.V0/Program.cs
+++ Tyuiu.BubenkoLG.Sprint0.Task5.V0/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tyuiu.BubenkoLG.Sprint0.Task5.V0.Lib;
 
 namespace Tyuiu.BubenkoLG.Sprint0.Task5.V0
 {
@@ -10,6 +11,12 @@ namespace Tyuiu.BubenkoLG.Sprint0.Task5.V0
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("A / B = " + DataService.Division(5, 0));
+            Console.ReadKey();
         }
     }
 }
